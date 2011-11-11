c94b31edc319d8f69d305e8ac90d230428e5ce0f
JJ Liu <jie.squared@gmail.com>
2011-11-10 23:51:10 -0800

    adf

--- 4-checkout/directions/index.php
+++ 4-checkout/directions/index.php
@@ -122,7 +122,6 @@ $address = $row['address'];
 <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script>
 
-
   </head>
   <body>
     <div id="control">
