6fb94959f781b430bbbddf16774e1d508a64133d
JJ Liu <jie.squared@gmail.com>
2011-11-10 23:48:38 -0800

    stylesheet

--- 4-checkout/choice.php
+++ 4-checkout/choice.php
@@ -115,7 +115,6 @@ $address = $row['address'];
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>
 
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.css" />
 <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script>
 
