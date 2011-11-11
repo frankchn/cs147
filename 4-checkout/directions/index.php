bbbd1876150ba86423cc877a5864b2dcd344f55d
JJ Liu <jie.squared@gmail.com>
2011-11-10 23:50:05 -0800

    meta

--- 4-checkout/directions/index.php
+++ 4-checkout/directions/index.php
@@ -121,6 +121,9 @@ $address = $row['address'];
     </script>
 <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script>
+<meta content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" name="viewport"/>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+
 
   </head>
   <body>
