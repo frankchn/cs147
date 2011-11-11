e281cfbd2ab77c1f152af6044f509ad103d17acc
JJ Liu <jie.squared@gmail.com>
2011-11-10 22:55:19 -0800

    dir

--- 4-checkout/directions/index.php
+++ 4-checkout/directions/index.php
@@ -38,21 +38,61 @@
         }
       }
     </style>
-    <script type="text/javascript"
-      src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
     <script type="text/javascript">
       var directionDisplay;
       var directionsService = new google.maps.DirectionsService();
 
       function initialize() {
         directionsDisplay = new google.maps.DirectionsRenderer();
+	var map;
         var myOptions = {
           zoom: 7,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           center: new google.maps.LatLng(41.850033, -87.6500523)
         };
-        var map = new google.maps.Map(document.getElementById('map_canvas'),
-            myOptions);
+        map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+
+	// Try HTML5 geolocation
+        if(navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                                             position.coords.longitude);
+
+            var infowindow = new google.maps.InfoWindow({
+              map: map,
+              position: pos,
+              content: 'Location found using HTML5.'
+            });
+
+            map.setCenter(pos);
+          }, function() {
+            handleNoGeolocation(true);
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleNoGeolocation(false);
+        }
+      }
+
+	function handleNoGeolocation(errorFlag) {
+        if (errorFlag) {
+          var content = 'Error: The Geolocation service failed.';
+        } else {
+          var content = 'Error: Your browser doesn\'t support geolocation.';
+        }
+
+        var options = {
+          map: map,
+          position: new google.maps.LatLng(60, 105),
+          content: content
+        };
+
+        var infowindow = new google.maps.InfoWindow(options);
+        map.setCenter(options.position);
+      }
+
+
         directionsDisplay.setMap(map);
         directionsDisplay.setPanel(document.getElementById('directions-panel'));
 
