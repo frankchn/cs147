7de3cced9716814742528bf4492cb056b50c20e2
JJ Liu <jie.squared@gmail.com>
2011-11-10 22:56:52 -0800

    geo

--- 4-checkout/directions/index.php
+++ 4-checkout/directions/index.php
@@ -53,55 +53,7 @@
         };
         map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
 
-	// Try HTML5 geolocation
-        if(navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = new google.maps.LatLng(position.coords.latitude,
-                                             position.coords.longitude);
-
-            var infowindow = new google.maps.InfoWindow({
-              map: map,
-              position: pos,
-              content: 'Location found using HTML5.'
-            });
-
-            map.setCenter(pos);
-          }, function() {
-            handleNoGeolocation(true);
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleNoGeolocation(false);
-        }
-      }
-
-	function handleNoGeolocation(errorFlag) {
-        if (errorFlag) {
-          var content = 'Error: The Geolocation service failed.';
-        } else {
-          var content = 'Error: Your browser doesn\'t support geolocation.';
-        }
-
-        var options = {
-          map: map,
-          position: new google.maps.LatLng(60, 105),
-          content: content
-        };
-
-        var infowindow = new google.maps.InfoWindow(options);
-        map.setCenter(options.position);
-      }
-
-
-        directionsDisplay.setMap(map);
-        directionsDisplay.setPanel(document.getElementById('directions-panel'));
-
-        var control = document.getElementById('control');
-        control.style.display = 'block';
-        map.controls[google.maps.ControlPosition.TOP].push(control);
-      }
-
-      function calcRoute() {
+     function calcRoute() {
         var start = document.getElementById('start').value;
         var end = document.getElementById('end').value;
         var request = {
