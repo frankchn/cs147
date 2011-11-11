63b21f7e578449d8cdc04197fc92007b6056c13b
JJ Liu <jie.squared@gmail.com>
2011-11-10 23:44:22 -0800

    blah

--- 4-checkout/directions/index.php
+++ 4-checkout/directions/index.php
@@ -115,6 +115,12 @@ $address = $row['address'];
       }
 
       google.maps.event.addDomListener(window, 'load', initialize);
+
+
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.css" />
+<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script>
+
     </script>
   </head>
   <body>
