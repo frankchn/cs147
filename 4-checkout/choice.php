7ff5a4d94d1963e7798987aae737731564b0a1bc
JJ Liu <jie.squared@gmail.com>
2011-11-10 23:58:26 -0800

    dir

--- 4-checkout/choice.php
+++ 4-checkout/choice.php
@@ -38,84 +38,14 @@ $address = $row['address'];
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>DormDecor</title>
 
-   <link href="style.css" rel="stylesheet" type="text/css">
-    <style type="text/css">
-      #directions-panel {
-        height: 100%;
-        float: right;
-        width: 390px;
-        overflow: auto;
-      }
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.css" />
+<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script>
 
-      #map-canvas {
-        margin-right: 400px;
-      }
-
-      #control {
-        background: #fff;
-        padding: 5px;
-        font-size: 14px;
-        font-family: Arial;
-        border: 1px solid #ccc;
-        box-shadow: 0 2px 2px rgba(33, 33, 33, 0.4);
-        display: none;
-      }
-
-      @media print {
-        #map-canvas {
-          height: 500px;
-          margin: 0;
-        }
-
-        #directions-panel {
-          float: none;
-          width: auto;
-        }
-      }
-    </style>
-    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
-    <script type="text/javascript">
-      var directionDisplay;
-      var directionsService = new google.maps.DirectionsService();
-
-      function initialize() {
-        directionsDisplay = new google.maps.DirectionsRenderer();
-	var map;
-        var myOptions = {
-          zoom: 7,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          center: new google.maps.LatLng(41.850033, -87.6500523)
-        };
-        map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-
-        directionsDisplay.setMap(map);
-        directionsDisplay.setPanel(document.getElementById('directions-panel'));
-
-        var control = document.getElementById('control');
-        control.style.display = 'block';
-        map.controls[google.maps.ControlPosition.TOP].push(control);
-      }
-
-      function calcRoute() {
-        //var start = document.getElementById('start').value;
-	var start = 'Stanford, CA';
-        var end = document.getElementById('end').value;
-        var request = {
-          origin: start,
-          destination: end,
-          travelMode: google.maps.DirectionsTravelMode.DRIVING
-        };
-        directionsService.route(request, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-          }
-        });
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-
-
+<meta content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" name="viewport"/>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="UTF-8">
+   
 </head>
 
 <body>
@@ -129,20 +59,9 @@ generate_header('Buy From Store', '<a data-ajax="false" data-transition="slideup
 		<b><?php echo $store_choice; ?></b><br><br>
 		which is located at <br><br>
 		<b><?php echo $address; ?></b><br>
+	<a href="directions.php?store=<?php echo $store_choice; ?>">See directions!</a></br>
+
         </div>
-	</div>
-<div id="control">
-      <strong>Start:</strong>
-      <select id="start" onchange="calcRoute();">
-        <option value="Stanford, CA">Stanford</option>
-      </select>
-      <strong>End:</strong>
-      <select id="end" onchange="calcRoute();">
-        <option value="<?php echo $address; ?>"><?php echo $store_choice; ?></option>
-      </select>
-    </div>
-    <div id="directions-panel"></div>
-    <div id="map_canvas"></div>
-</div> 
+</div>
 </body>
 </html>
